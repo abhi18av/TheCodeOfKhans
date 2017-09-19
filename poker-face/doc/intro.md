4f38777eea984617a42db461fec0588b15345368
Abhinav Sharma <abhi18av@gmail.com>
2017-09-19 11:22:35 +0530

    added the context to develop basic games using card play

---
+++ poker-face/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to poker-face
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ poker-face/test/poker_face/core_test.clj
@@ -0,0 +1,7 @@
+(ns poker-face.core-test
+  (:require [clojure.test :refer :all]
+            [poker-face.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
